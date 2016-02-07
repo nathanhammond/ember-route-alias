ebc6e5d72c61e6480852f1d4781765aea6dc815d
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-02-07 19:41:02 +0100

    ember-try: Add "ember-1.13" scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,6 +6,12 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.13',
+      dependencies: {
+        'ember': '~1.13.0'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
